12f3f39db4e72e763b7cae783eefe02e6afcf0aa
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-09-07 14:57:11 +1000

    add httpClient in module

--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
@@ -21,7 +21,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [SocketService],
   bootstrap: [AppComponent]
--- chat/src/app/login/login.component.ts
+++ chat/src/app/login/login.component.ts
@@ -23,9 +23,47 @@ export class LoginComponent implements OnInit {
   user = {'userid': '', 'password': ''}
   // constructor(private router:Router, private httpClient: HttpClient) { }
 
-constructor(private router:Router ){}
+constructor(private router:Router, private httpClient: HttpClient ){}
 
   ngOnInit(): void {
   }
+  login(){
+    console.log('user inputs:', this.userid, this.password);
+    console.log('login clicked');
 
+    // let user = {'username': this.userid, 'password': this.password, 'birthdate': null, 'age':null,'email':null, 'valid':false}
+    // console.log('default value:' + user);
+
+    // this.httpClient.post(BACKEND_URL + '/api/login', user,  httpOptions).subscribe((data:any)=>{
+    // this.httpClient.post(BACKEND_URL + '/api/login', user).subscribe(res=>
+    //   {console.log(res);},);
+
+    let userLogin = {'username': this.userid, 'password': this.password}
+    this.httpClient.post(BACKEND_URL + '/api/login', userLogin).subscribe((data: any)=>{
+      console.log(data)
+      console.log(data.username)
+      console.log(data.password)
+      console.log(data.valid)
+
+      console.log(data.email)
+      console.log(data.age)
+
+
+      // console.log(sessionStorage)
+
+      // alert(JSON.stringify(this.password));
+      if (data.valid) {
+        sessionStorage.setItem('username', this.userid);
+        sessionStorage.setItem('birthdate', data.birthdate);
+        sessionStorage.setItem('age', data.age);
+        sessionStorage.setItem('email', data.email);
+        sessionStorage.setItem('valid', data.valid);
+        console.log('valid user' + sessionStorage);
+        this.router.navigateByUrl('/account/' + this.userid);
+
+      } else { alert("email or password incorrect");}
+    }
+    );
+    
+  }
 }
