53f7bfbbd5c3d71aa37f195064846d58d01eb742
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-09-07 12:35:31 +1000

    app setup & move server under chat folder

--- chat/src/app/chat/chat.component.ts
+++ chat/src/app/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import* as io from 'socket.io-client';
+import { SocketService } from '../services/socket.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  messagecontent  : string = '';
+  errorMassage: string = '';
+  messages:string[] = [];
 
-  constructor() { }
+  ioConnection:any;
+
+  constructor(private socketService:SocketService){}
+  // constructor() { 
+  //   this.socket = io(this.url);
+  //   this.socket.on('new-messages':m=>{alert(m);});
+  // }
 
   ngOnInit(): void {
+    this.initIoConnection();
+  }
+  private initIoConnection(){
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService.getMessage()
+      .subscribe((message:any)=>{
+      this.messages.push(message);
+    });
   }
 
+  chat(){
+    if(this.messagecontent){
+      this.socketService.send(this.messagecontent);
+      this.messagecontent = '';
+    }else{
+      console.log('no message');
+    }
+  }
 }
--- chat/src/app/services/socket.service.ts
+++ chat/src/app/services/socket.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io } from 'socket.io-client';
+
+const BACKENT_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
-
+  private socket: any;
   constructor() { }
+
+  initSocket(){
+    this.socket = io(BACKENT_URL);
+    return ()=>{this.socket.disconnetc();}
+  }
+
+  send(message: string){
+    this.socket.emit('message', message)
+  }
+
+  getMessage(){
+    return new Observable(observer=>{
+      this.socket.on('message', (data: any)=>{observer.next(data)
+      });
+    });
+  }
 }
