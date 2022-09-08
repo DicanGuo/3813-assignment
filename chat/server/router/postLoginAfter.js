c39441813a161950cd28c4af27e4075f661fa3c3
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-09-08 21:16:25 +1000

    create user credencial in JSON

--- chat/server/router/postLoginAfter.js
+++ chat/server/router/postLoginAfter.js
@@ -8,6 +8,7 @@ module.exports = function(req, res) {
         "email": req.body.email
     }
     let uArray = [];
+    let loginArray = [];
     fs.readFile('./data/extendedUsers.json', 'utf8', function(err, data) {
         //open the file of user list
         if (err) throw err;
@@ -30,7 +31,6 @@ module.exports = function(req, res) {
                     userobj.userid = k;
                     break
                 }
-                
             }
             uArray.push(userobj);
 
@@ -48,6 +48,32 @@ module.exports = function(req, res) {
         // save the file of user list
         let uArrayjson = JSON.stringify(uArray);
         fs.writeFile('./data/extendedUsers.json', uArrayjson, 'utf-8', function(err) {
+            if (err) throw err;
+        });
+    });
+
+    fs.readFile('./data/users.json', 'utf8', function(err, data) {
+        //open the file of user list
+        if (err) throw err;
+        loginArray = JSON.parse(data);
+        console.log(loginArray);
+        // allLoginArray = loginArray;
+        // console.log(allLoginArray)
+        // make some change according to user's post 
+        let i = loginArray.findIndex(x => x.username == userobj.username);
+        let newUser = {username: userobj.username, email: userobj.email};
+
+        if (i == -1) {
+            loginArray.push(newUser);
+        } else {
+            alert('User already exist')
+        }
+
+        // send response to user
+        // res.send(uArray);
+        // save the file of user list
+        let uArrayjson = JSON.stringify(loginArray);
+        fs.writeFile('./data/users.json', uArrayjson, 'utf-8', function(err) {
             if (err) throw err;
         });
     });
