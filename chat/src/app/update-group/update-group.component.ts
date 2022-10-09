9b95b09c5cae4752657e2d340a0dd4b67ac1bf98
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-10-09 16:05:42 +1000

    styl edit group

--- chat/src/app/update-group/update-group.component.ts
+++ chat/src/app/update-group/update-group.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Route } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-update-group',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-group.component.css']
 })
 export class UpdateGroupComponent implements OnInit {
-
-  constructor() { }
+  id = undefined;
+  groupadmin = [];
+  groupassis = [];
+  groupusers = [];
+  constructor(private router:Router, private httpClient: HttpClient ) { 
+  }
 
   ngOnInit(): void {
+    this.init();
+  }
+
+  init(){
+    let targetgroup = JSON.parse(localStorage.getItem('targetGroup')!);
+    this.id = targetgroup.id;
+    this.groupadmin = targetgroup.groupadmin;
+    this.groupassis = targetgroup.groupassis;
+    this.groupusers = targetgroup.groupusers;
+  }
+
+  cancel(){
+    localStorage.removeItem('targetGroup');
+    this.router.navigateByUrl("/group");
+
   }
 
 }
