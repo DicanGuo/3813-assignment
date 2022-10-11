bebe93fb2276f75020cad5e508adcbec283942ef
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-10-11 19:29:06 +1000

    update channel

--- chat/server/routes/updateChannel.js
+++ chat/server/routes/updateChannel.js
@@ -1,67 +1,58 @@
 module.exports = function(app, db, ObjectID){
-    // app.put('/api/updateChannel', (req, res)=>{
+    app.put('/api/updateChannel', (req, res)=>{
     //     const assert = require('assert');
-    //     var _id = req.body._id;
-    //     let ObjectId = new ObjectID(_id);
-    //     var id = req.body.id;
-    //     var groupusers = req.body.groupusers;
-    //     var groupassis = req.body.groupassis;
+        var _id = req.body._id;
+        let ObjectId = new ObjectID(_id);
+        var id = req.body.id;
+        var channelusers = req.body.channelusers;
+        var groupid = req.body.groupid;
     //     var groupadmin = req.body.groupadmin;
     //     console.log("Received: " + id,groupadmin,groupassis,groupusers)
     //     // console.log(_id)
 
-    //     const collection = db.collection('groups');
-    //     const collection2 = db.collection('credentials');
-    //     let list = [];
-    //     let userList = [];
-    //     let count = 0;
-    //     for(i in groupusers){
-    //         if(!(groupusers[i] == '')){
-    //             list.push(groupusers[i])
-    //         }
-    //     }
-    //     for(i in groupassis){
-    //         if(!(groupassis[i] == '')){
-    //             list.push(groupassis[i])
-    //         }
-    //     }
-    //     for(i in groupadmin){
-    //         if(!(groupadmin[i] == '')){
-    //             list.push(groupadmin[i])
-    //         }
-    //     }
-    //     // console.log('user list: '+list)
-
-    //     collection2.find().toArray((err,users)=>{
-    //         for(i in users){
-    //             // console.log(users[i])
-    //             // console.log(i)
-    //             userList.push(users[i].name);
-    //         };
-    //         console.log(userList)
-    //         for(i in list){
-    //                 if(!(userList.includes(list[i]))){
-    //                     count += 1;
-    //                 }
-    //         }
+        const collection = db.collection('channels');
+        const collection2 = db.collection('credentials');
+        let list = [];
+        let userList = [];
+        let count = 0;
+        collection2.find().toArray((err,users)=>{
+            for(i in users){
+                // console.log(users[i])
+                // console.log(i)
+                userList.push(users[i].name);
+            };
+            // console.log(userList)
+            for(i in channelusers){
+                if(!(channelusers[i] == '')){
+                    list.push(channelusers[i])
+                }
+            }
+            for(i in list){
+                    if(!(userList.includes(list[i]))){
+                        count += 1;
+                    }
+            }
     //         console.log(count)
-    //         if(count > 0){
-    //             res.send({'ok':false, 'message':'User not exists'})
-    //         }else{
-    //             query = {'_id':ObjectId, 'id':id, 'groupusers':groupusers, 'groupassis':groupassis, 'groupadmin':groupadmin}
-    //             collection.updateOne({'_id':ObjectId}, {$set:query}, (err,data)=>{
-    //                 console.log(data)
-    //                 collection.find({}).toArray((err,data)=>{
-    //                     console.log(data)
-    //                     let sendData = {};
-    //                     sendData['groups']=data;
-    //                     sendData['ok']=true;
-    //                     sendData['message']='Group updated';
-    //                     res.send(sendData)
-    //                 })
-    //             })
-    //         }
+            if(count > 0){
+                res.send({'ok':false, 'message':'User not exists'})
+            }else{
+                query = {'_id':ObjectId, 'id':id, 'groupid':groupid, 'channelusers':channelusers}
+                collection.updateOne({'_id':ObjectId}, {$set:query}, (err,data)=>{
+                    console.log(data)
+                    collection.find({}).toArray((err,data)=>{
+                        console.log(data)
+                        let sendData = {};
+                        sendData['channels']=data;
+                        sendData['ok']=true;
+                        sendData['message']='channel updated';
+                        collection.find({'_id':ObjectId}).toArray((err,target)=>{
+                            sendData['targetChannel']=target;
+                            res.send(sendData);
+                        })
+                    })
+                })
+            }
 
-    //     })
-    // });
+        })
+    });
 };
--- chat/src/app/update-channel/update-channel.component.ts
+++ chat/src/app/update-channel/update-channel.component.ts
@@ -13,11 +13,107 @@ const BACKEND_URL = 'http://localhost:3000';
   styleUrls: ['./update-channel.component.css']
 })
 export class UpdateChannelComponent implements OnInit {
+  newUser: String = '';
+  // channel
+  _id = undefined;
+  id = undefined;
+  groupid= undefined;
+  channelusers: String[] = [];
+  targetChannel = JSON.parse(localStorage.getItem('targetChannel')!);
 
   constructor(private router:Router, private httpClient: HttpClient ) { 
   }
   ngOnInit(): void {
+    this.init();
+
   }
+
+  init(){
+    this._id = this.targetChannel._id;
+    this.id = this.targetChannel.id;
+    this.groupid = this.targetChannel.groupid;
+    this.channelusers = this.targetChannel.channelusers;
+    console.log(this.channelusers)
+
+  }
+
+  add(){
+    if(this.newUser == ''){
+      alert('Input is empty !');
+      // this.refreshWindow();
+    }else{
+      console.log(this.channelusers)
+      console.log(this.newUser)
+
+      if(this.channelusers.includes(this.newUser)){
+        alert('User already in channel !');
+      }else {
+        this.channelusers.push(this.newUser);
+        console.log(this.channelusers);
+        this.update();
+      }
+    }
+  }
+
+  deleteUser(targetUser:any){
+
+    for(let i=0;i< this.channelusers.length;i++){
+      console.log(this.channelusers[i])
+      if(targetUser == this.channelusers[i]){
+        this.channelusers.splice(i,1)
+      }
+    }
+    console.log(this.channelusers)
+    this.update();
+  
+  }
+
+  update(){
+    let tChannel = {'_id':this._id, 'id':this.id, 'groupid':this.groupid, 'channelusers': this.channelusers}
+    
+    // console.log(newUser);
+    this.httpClient.put(BACKEND_URL + '/api/updateChannel', tChannel,  httpOptions)
+      .subscribe((receivedData: any) => {
+        // alert(JSON.stringify(data));
+        console.log(receivedData)
+        if(receivedData.ok){
+          alert(receivedData.message)
+          let data = receivedData.channels
+          console.log(data)
+          localStorage.setItem('channels', JSON.stringify(receivedData.channels));
+
+          // this.refreshWindow();
+
+        //   for(let i=0; i< this.groups.length;i++){
+        //     // console.log(this.groups[i]._id )
+        //     for(let j=0; j< data.length;j++){
+        //       if(targetGroup._id == data[j]._id){
+        //         // console.log('targetGroup._id'+targetGroup._id, 'data[j]._id'+data[j]._id)
+        //         localStorage.setItem('targetGroup', JSON.stringify(data[j]));
+
+        //       }
+        //     }
+          // }
+
+        //   // localStorage.setItem('groups', JSON.stringify(data.uArray));
+        //   alert(JSON.stringify(receivedData.message));
+        //   this.refreshWindow();
+        } else {
+          alert('failed: ' + JSON.stringify(receivedData.message));
+          this.refreshWindow();
+        }
+      });
+    localStorage.setItem('targetChannel', JSON.stringify(tChannel));
+
+    this.targetChannel = JSON.parse(localStorage.getItem('targetChannel')!);
+
+    
+  };
+
+  back(){
+    this.router.navigateByUrl('/group/' + this.groupid + '/channels');
+  }
+
   refreshWindow(){
     window.location.reload();
   };
